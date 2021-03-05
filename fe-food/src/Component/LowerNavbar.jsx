2547a71e6d1b428a9b88bca1f75cffb855f9c52e
alihusnain4190 <alihusnain4190@gmail.com>
2021-03-05 12:31:44 +0000

    add some css for Navbar and lowernav

--- fe-food/src/Component/LowerNavbar.jsx
+++ fe-food/src/Component/LowerNavbar.jsx
@@ -2,11 +2,16 @@ import React from "react";
 
 const LowerNavbar = () => {
   return (
-    <section>
+    <section className="lowernavbar">
       <div>
+        <i class="dpg-icon dpg-icon-menu-filter-veg"></i>
         <p>Vagitarian</p>
+      </div>
+      <div>
         <p>Vigin</p>
+      </div>
 
+      <div>
         <p>Hot</p>
       </div>
     </section>
--- fe-food/src/Component/Navbar.jsx
+++ fe-food/src/Component/Navbar.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { FaCartPlus } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>
-        <p>Pizza</p>
-        <p>Drinks</p>
+    <nav className="navbar">
+      <div className="navbar__text">
+        <a href="/">Pizza</a>
+        <a href="#">Drinks</a>
       </div>
-      <div>Cart</div>
+      <div className="navbar__cart">
+        <FaCartPlus className="cart" />
+      </div>
     </nav>
   );
 };
