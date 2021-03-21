1d8c09d8b298acd04fa668cf66a598a89c30f212
alihusnain4190 <alihusnain4190@gmail.com>
2021-03-21 16:04:45 +0000

    delete be

--- be-food/db/connection.js
+++
@@ -1,9 +0,0 @@
-const ENV = process.env.NODE_ENV || "development";
-const connection = require("knex");
-
-const dbConfig =
-  ENV === "production"
-    ? { client: "pg", connection: process.env.DATABASE_URL }
-    : require("../Knexfile");
-
-module.exports = connection(dbConfig);
